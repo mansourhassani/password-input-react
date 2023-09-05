2852e99fe1d7215787802bb61291066e5bd1f29a
mansourhassani <mansour.hasani.dev@gmail.com>
2023-09-05 20:22:36 +0330

    Initialize project, configuration and installation of necessary packages.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { IoAirplaneOutline } from 'react-icons/io5';
+function App() {
+	return (
+		<div className="App">
+			تست
+			<button type="button" className="btn btn-success">
+				Success
+			</button>
+			<IoAirplaneOutline />
+		</div>
+	);
+}
+
+export default App;
